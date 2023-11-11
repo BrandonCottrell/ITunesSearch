b214a3ee013956a769423e0812df96ce2f9e02e3
Brandon Cottrell <bcottrell@my.wctc.edu>
2023-11-10 22:42:34 -0600

    iTunes search

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 
-    // let searchTerm = $("myInput").val();
+    var searchThis = prompt("What would you like to look up?")
+    
     let endpoint = 'https://itunes.apple.com/search';
     let params = {
-        term: searchTerm,
+        term: searchThis,
         // filter: 'full',
-        //limit: 5
+        limit: 20
+        
     }
 
     
@@ -16,17 +18,8 @@ $(document).ready(function(){
         function(data){
             data.results.forEach(result => {
                 $('#result').append(`<h4>Title: <h6>${result.collectionName ?? result.trackName}<h6> </h4>`);
-                $('#result').append(`<h4>Media Type: <h6>${result.wrapperType}</h6> </h4>`);
+                $('#result').append(`<h4>Media Type: <h6>${result.kind ?? result.wrapperType}</h6> </h4>`);
                 $('#result').append(`<h4>Artist Name: <h6> ${result.artistName ?? 'N/A'} </h6></h4><hr>`);
-                
-                // $('#result').append(`<h3>${result.trackName}</h3><hr>`);
-                // if(item.volumeInfo.authors){
-                //     $('#result').append(`<p>By: ${item.volumeInfo.authors.join(', ')}</p>`);
-                // }
-                
-                // $('#result').append(`<p>By: ${item.volumeInfo?.authors?.join(', ') ?? 'N/A'}</p>`);
-
-                // $('#result').append(`<img src="${item.volumeInfo?.imageLinks?.thumbnail}"><hr>`);
             })
         },
         'json'
